e44f460e0f2c5e1a89486905659820e5fc058202
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-03-27 07:23:45 +0600

    shop

---
+++ src/Component/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './shop.css'
+
+const Shop = () => {
+  return (
+    <div className='shop-container'>
+      <div className="product-container">
+        <h2> this is product</h2>
+      </div>
+      <div className="cart-container">
+        <h2> this is cart </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
